e9b6f81b0d1c04f0c4d41c1ea1dd1860cdfdbebf
April Trull <mtwoolybunnies@gmail.com>
2017-10-24 20:43:04 -0600

    yuppity

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ContactForm from './contactForm.js';
 import '../assets/stylesheets/contact.css';
-import Quilts from '../assets/img/name.png';
+
 
 export default class Contact extends React.Component {
 
@@ -11,7 +11,7 @@ export default class Contact extends React.Component {
           <h1 className="contactHeader orange"> Contact For Quotes or Questions </h1>
             <div className="contactContent">
               <div className="contactFlex">
-                // <img src={Quilts} alt = "nickname of img" />
+                
                 <h4> <a href="tel:406.273.2050">406.273.2050</a> <br/>
                 <a href="http://rnwilcox51@gmail.com">rnwilcox51@gmail.com</a> </h4>
                 <br/>
