6baaccc4daae8571bde00f639590963e8bae2a6d
April Trull <mtwoolybunnies@gmail.com>
2017-10-24 20:55:29 -0600

    working but need to get email to work

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,9 +11,9 @@ export default class Contact extends React.Component {
           <h1 className="contactHeader orange"> Contact For Quotes or Questions </h1>
             <div className="contactContent">
               <div className="contactFlex">
-                
+
                 <h4> <a href="tel:406.273.2050">406.273.2050</a> <br/>
-                <a href="http://rnwilcox51@gmail.com">rnwilcox51@gmail.com</a> </h4>
+                <a href="http://bnbinmt1@gmail.com">bnbinmt1@gmail.com</a> </h4>
                 <br/>
               </div>
 
