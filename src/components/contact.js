29837dd415fe0946eab2d51363359f78d4c6ef87
April Trull <mtwoolybunnies@gmail.com>
2017-10-25 13:00:28 -0600

    my typing mistakes make it look like I cant spell - ugh

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,7 +11,7 @@ export default class Contact extends React.Component {
           <h1 className="contactHeader orange"> Contact For Quotes or Questions </h1>
             <div className="contactContent">
               <div className="contactFlex">
-                <img src={headerImg}/>
+                <img src={headerImg}alt=" "/>
                 <h4> <a href="tel:406.273.2050">(406)273-2050</a> <br/>
                 <a href="http://bnbinmt1@gmail.com">bnbinmt1@gmail.com</a> </h4>
                 <br/>
