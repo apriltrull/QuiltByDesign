c72ef0a0cf7aa4ae349de07affa8c7a8a19389c2
April Trull <mtwoolybunnies@gmail.com>
2017-10-25 12:32:56 -0600

    trying to add img

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ContactForm from './contactForm.js';
 import '../assets/stylesheets/contact.css';
-
+import headerImg from '../assets/stylesheets/main.css'
 
 export default class Contact extends React.Component {
 
@@ -11,8 +11,8 @@ export default class Contact extends React.Component {
           <h1 className="contactHeader orange"> Contact For Quotes or Questions </h1>
             <div className="contactContent">
               <div className="contactFlex">
-
-                <h4> <a href="tel:406.273.2050">406.273.2050</a> <br/>
+                <img src={headerImg} alt = "barn header"/> 
+                <h4> <a href="tel:406.273.2050">(406)273-2050</a> <br/>
                 <a href="http://bnbinmt1@gmail.com">bnbinmt1@gmail.com</a> </h4>
                 <br/>
               </div>
