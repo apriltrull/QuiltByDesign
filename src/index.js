0daa72951bf95ed2565029def7eec3648dcccfe8
April Trull <mtwoolybunnies@gmail.com>
2017-10-26 13:04:55 -0600

    yippity

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import Main from './components/main.js';
 import Contact from './components/contact.js';
 import './assets/stylesheets/index.css';
+import Progress from './components/progress.js';
 
 class Grit extends React.Component {
   render() {
     return (
       <div>
         <Main />
-
+        <Progress/>
         <div className="statsMainContainer">
           <h1 className='orange stats'> Professional Design, Your Way </h1>
             <div className="flexWrapper">
