3316f6bcfc35aabc2aeb57a41fc642f26475f652
April Trull <mtwoolybunnies@gmail.com>
2017-10-26 14:18:16 -0600

    yeah diddy

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -11,8 +11,8 @@ export default class Introduction extends React.Component {
         <div className="intro">
 
             <div>
-            <img src ={Becky} alt="becky with longarm" style={width:100%}/>
-              <h1 className="grit" style={text-align:right}>Becky Wilcox</h1>
+            <img src ={Becky} alt="becky with longarm"/>
+              <h1 className="grit" >Becky Wilcox</h1>
               <h2>Becky has been longarm quilting since 2002.  etc etc etc</h2>
             </div>
 
