2ce6c85603191c887f8fbda1833f27e07c322762
April Trull <mtwoolybunnies@gmail.com>
2017-10-26 10:07:53 -0600

    working on images

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import '../assets/stylesheets/progress.css';
 import ScrollAnimation from 'react-animate-on-scroll';
-import girlsBike from '../assets/img/GRIT_girls_bike.jpg';
-import girlsChopsaw from '../assets/img/GRIT_girls_chopsaw.jpg';
-import girlsWelding from '../assets/img/GRIT_girls_welding.jpg';
-import girlsSaw from '../assets/img/GRIT_girls_circularsaw.jpg';
-import girlsDrill from '../assets/img/GRIT_girls_drill.jpg';
-import girlAtTableSaw from '../assets/img/GRIT_girl_chopsaw.jpg';
-import gritBike from '../assets/img/GRIT_bike.jpg';
-import girlLumber from '../assets/img/GRIT_girl_lumber.jpg';
-import gritSaw from '../assets/img/GRIT_girls_saw.jpg';
-import groupSaw from '../assets/img/GRIT_group_saw.jpg';
-import weldFlash from '../assets/img/GRIT_welding_flash.jpg';
+
 
 export default class Progress extends React.Component {
   constructor() {
@@ -97,27 +87,27 @@ export default class Progress extends React.Component {
 
             <div className="mySlides fade">
               <div className="numbertext">6 / 10</div>
-              <img src={girlAtTableSaw} alt="girl working with table saw" style={{width:'100%'}} />
+              <img src=/home/osboxes/Dropbox/20150216_1069 (copy).jpg alt = "large square"  style={{width:'100%'}} />
             </div>
 
             <div className="mySlides fade">
               <div className="numbertext">7 / 10</div>
-              <img src={gritBike} alt="girl fixing a bike seat" style={{width:'100%'}} />
+              <img src=/home/osboxes/Dropbox/20130417_0284 (copy).jpg alt = "single square, light green"  style={{width:'100%'}} />
             </div>
 
             <div className="mySlides fade">
               <div className="numbertext">8 / 10</div>
-              <img src={girlLumber} alt="girl moving a wood beam" style={{width:'100%'}} />
+              <img src=/home/osboxes/Dropbox/20110129_1031.jpg alt = "single pink flower" style={{width:'100%'}} />
             </div>
 
             <div className="mySlides fade">
               <div className="numbertext">9 / 10</div>
-              <img src={gritSaw} alt="girl working with table saw" style={{width:'100%'}} />
+              <img src=/home/osboxes/Dropbox/20110111_0879 (copy).jpg alt="single square" style={{width:'100%'}} />
             </div>
 
             <div className="mySlides fade">
               <div className="numbertext">10 / 10</div>
-              <img src={weldFlash} alt="girl welding with sparks flying" style={{width:'100%'}} />
+              <img src=/home/osboxes/Dropbox/20110111_0881 (copy).jpg alt = "single square with flowers" style={{width:'100%'}} />
             </div>
 
             <a className="prev" onClick={this.minusSlidesClick}>&#10094;</a>
