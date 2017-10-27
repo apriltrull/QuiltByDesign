33ec94bba2ddbf060eb03d5abdda983f42c0ae2f
April Trull <mtwoolybunnies@gmail.com>
2017-10-27 15:51:29 -0600

    yeah

--- src/components/main.js
+++ src/components/main.js
@@ -8,7 +8,7 @@ export default class Main extends React.Component {
 
         <div className="headerImg">
 
-            <div>
+            <div className = "header">
               <h1 className="quilt">Quilting With Class</h1>
               <h2 className = "extend">.....Professional Longarm Quilting by Becky Wilcox</h2>
             </div>
