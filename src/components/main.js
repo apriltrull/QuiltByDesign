e949ab77ad6d416cf0690c3dee6acaf47ad80f9d
April Trull <mtwoolybunnies@gmail.com>
2017-10-28 15:15:13 -0600

    still at it

--- src/components/main.js
+++ src/components/main.js
@@ -11,8 +11,8 @@ export default class Main extends React.Component {
 
             <div className = "header">
 
-              <h1 className="quilt"  style={ font-family: Ribbon Cursive; }>Quilting With Class</h1>
-              <h2 className = "extend" style ={{font-family:Ribbon Cursive;}}>.....Professional Longarm Quilting by Becky Wilcox</h2>
+              <h1 className="quilt">Quilting With Class</h1>
+              <h2 className = "extend" >{ font-family: Ribbon Cursive; }.....Professional Longarm Quilting by Becky Wilcox</h2>
             </div>
 
         </div>
