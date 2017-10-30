f03659429c928d8b60062d5c2d5a02589a2e6770
April Trull <mtwoolybunnies@gmail.com>
2017-10-30 09:20:41 -0600

    working now

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import  '../assets/stylesheets/main.css';
+import WebFont from 'webfontloader';
 
+WebFont.load({
+  google: {
+    families: ['Ribbon Cursive:300,400,700', 'cursive']
+  }
+});
 
 export default class Main extends React.Component {
 
