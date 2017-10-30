4af9dc9dfd086e5c3cdf20aa4f1a52898c08196c
April Trull <mtwoolybunnies@gmail.com>
2017-10-30 14:49:08 -0600

    working on animated slide show

--- src/components/progress.js
+++ src/components/progress.js
@@ -28,41 +28,198 @@ import TwoBettys from '../assets/img/twoBettys.jpg';
 import TwoBirds from '../assets/img/twoBirds.jpg';
 import YellowSquare from '../assets/img/yellowSquare.jpg';
 
-import { Slide } from 'react-slideshow-image';
-import { Fade } from 'react-slideshow-image';
-
-
-const images = [
-  'img/mallard.jpg',
-  'img/swan.jpg',
-  'img/flyingSwan.jpg',
-  'img/loons.jpg',
-  'img/ants.jpg',
-  'img/blueButterfly.jpg'
-];
-
 
 
 export default class Progress extends React.Component {
+  constructor() {
+    super();
+    this.plusSlidesClick = this.plusSlides.bind(this);
+    this.minusSlidesClick = this.minusSlides.bind(this);
+    this.state = {
+      slideIndex:1
+    }
+  }
 
+  componentDidMount() {
+    this.showSlides();
+  }
 
+  componentDidUpdate() {
+    this.showSlides();
+    return true;
+  }
+
+  plusSlides() {
+   let nextIndex;
+   if(this.state.slideIndex < 25) {
+     nextIndex = this.state.slideIndex + 1;
+   } else {nextIndex = this.state.slideIndex}
+   this.setState({
+     slideIndex: nextIndex
+   });
+  }
+
+  minusSlides() {
+    let nextIndex;
+    if(this.state.slideIndex > 1) {
+      nextIndex = this.state.slideIndex - 1;
+    } else {nextIndex = this.state.slideIndex}
+    this.setState({
+      slideIndex: nextIndex
+    });
+  }
+
+  showSlides() {
+      var i;
+      var slideIndex = this.state.slideIndex;
+      var slides = document.getElementsByClassName("mySlides");
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      slides[slideIndex-1].style.display = "block";
+    }
 
   render () {
-
-    const Slideshow = () => {
-        return (
-
+    return (
       <div className='progress'>
         <h1 className='orange'> Quilt Gallery </h1>
+        <br/>
+
+        <div className="slideshow-container">
+
+            <div className="mySlides fade">
+              <div className="numbertext">1 / 25</div>
+              <img src={TexasSun} alt="texas sun shape" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">2 / 25</div>
+              <img src={Mallard} alt="mallard bird" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">3 / 25</div>
+              <img src={Swan} alt="swimming swan" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">4 / 25</div>
+              <img src={FlyingSwan} alt="swan flying" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">5 / 25</div>
+              <img src={Loons} alt="swimming loons" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">6 / 25</div>
+              <img src={Ants} alt = "trail of ants"  style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">7 / 25</div>
+              <img src={BlueButterfly} alt = "blue butterfly"  style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">8 / 25</div>
+              <img src={Cats}alt = "cat quilt" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">9 / 25</div>
+              <img src={Checkerboard} alt="x checkerboard" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">10 / 25</div>
+              <img src={GreenSquare}alt = "light green square" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">11 / 25</div>
+              <img src={YellowSquare}alt = "yellow square" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">12 / 25</div>
+              <img src={BettyBonnet}alt = "bettyBonnet" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">13 / 25</div>
+              <img src={FullBettys}alt = "three betty bonnets" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">14 / 25</div>
+              <img src={GreenStar}alt = "green star" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">15 / 25</div>
+              <img src={Home}alt = "home sweet home quilt" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">16 / 25</div>
+              <img src={LargeGreen}alt = "large green square" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">17 / 25</div>
+              <img src={LargePurple}alt = "large purple square" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">18 / 25</div>
+              <img src={Butterfly} alt = "large butterfly" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">19 / 25</div>
+              <img src={ Pink }alt = "pink flower" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">20 / 25</div>
+              <img src={Purple}alt = "purple circle" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">21 / 25</div>
+              <img src={PurpleSquare}alt = "purple square" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">22 / 25</div>
+              <img src={Red}alt = "red square" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">23 / 25</div>
+              <img src={Smallfly}alt = "small butterfly" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">24 / 25</div>
+              <img src={TwoBettys}alt = "two betty bonnets" style={{width:'100%'}} />
+            </div>
+
+            <div className="mySlides fade">
+              <div className="numbertext">25 / 25</div>
+              <img src={TwoBirds}alt = "two birds on catails" style={{width:'100%'}} />
+            </div>
 
 
 
-        <Fade
-          img={images}
-          duration="5000"
-          transitionDuration="1000" />
+            <a className="prev" onClick={this.minusSlidesClick}>&#10094;</a>
+            <a className="next" onClick={this.plusSlidesClick}>&#10095;</a>
+         </div>
+         <br/>
+
       </div>
-     );
-   }
- };
+    );
+  }
 };
