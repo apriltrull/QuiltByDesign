146fb843ce6eb6c2c1e22a2d9e528f58b8319423
April Trull <mtwoolybunnies@gmail.com>
2017-11-01 09:53:01 -0600

    adding top navbar

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Progress from './components/progress.js';
 import Introduction from './components/introduction.js';
 import Tips from './components/tips.js';
 import Testimonials from './components/testimonials.js';
-
+import Header from './components/header.js';
 
 class Grit extends React.Component {
   render() {
