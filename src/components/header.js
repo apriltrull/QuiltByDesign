56cd9c2a3255fb9b706caf866da8e1a83e015589
April Trull <mtwoolybunnies@gmail.com>
2017-11-01 10:29:43 -0600

    adding top navbar

--- src/components/header.js
+++ src/components/header.js
@@ -9,14 +9,14 @@ export default class Header extends React.Component {
 
       <header className="navContainer">
 
-        <div className="header">
+        <div className="row1">
 
 
-          <Link to="/introduction"><li>Quilting Professionals</li></Link>
-          <Link to="/progress"><li>Quilt Gallery</li></Link>
-          <Link to="/testimonials"><li>Testimonials</li></Link>
-          <Link to="/tips"><li>Quilting Tips</li></Link>
-          <Link to="/contact"><li>Contact</li></Link>
+          <Link to="./introduction"><li>Quilting Professionals</li></Link>
+          <Link to="./progress"><li>Quilt Gallery</li></Link>
+          <Link to="./testimonials"><li>Testimonials</li></Link>
+          <Link to="./tips"><li>Quilting Tips</li></Link>
+          <Link to="./contact"><li>Contact</li></Link>
 
 
          </div>
