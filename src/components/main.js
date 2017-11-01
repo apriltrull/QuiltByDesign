fa92bdf5518ac3062434335eda614e36fa4764e9
April Trull <mtwoolybunnies@gmail.com>
2017-11-01 12:00:29 -0600

    connecting links for navbar

--- src/components/main.js
+++ src/components/main.js
@@ -10,6 +10,23 @@ export default class Main extends React.Component {
 
         <div className="headerImg">
 
+        <header className="navContainer">
+         <ul>
+          <div className="row1">
+
+
+            <Link to="/introduction"><li>Quilting Professionals</li></Link>
+            <Link to="/progress"><li>Quilt Gallery</li></Link>
+            <Link to="/testimonials"><li>Testimonials</li></Link>
+            <Link to="/tips"><li>Quilting Tips</li></Link>
+            <Link to="/contact"><li>Contact</li></Link>
+
+
+            </div>
+           </ul>
+          </header>
+
+
             <div className = "advertise">
 
               <h1 className="quilt">Quilting With Class</h1>
