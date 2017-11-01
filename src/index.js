235753c91a51392bb96b9afc9743d536f070600e
April Trull <mtwoolybunnies@gmail.com>
2017-11-01 11:58:06 -0600

    connecting links for navbar

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import Progress from './components/progress.js';
 import Introduction from './components/introduction.js';
 import Tips from './components/tips.js';
 import Testimonials from './components/testimonials.js';
-import Heading from './components/heading.js';
+
 
 class Grit extends React.Component {
   render() {
     return (
       <div>
-       <Heading/>
+
         <Main />
         <Introduction/>
 
