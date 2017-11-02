bb37f49a969f481a61d451e438cc0a5415714c9c
April Trull <mtwoolybunnies@gmail.com>
2017-11-02 12:07:35 -0600

    navbar in place, working on styling for navbar

--- src/components/heading.js
+++ src/components/heading.js
@@ -8,23 +8,23 @@ export default class NavComponent extends React.Component {
 			<nav>
 				<div className="navWide">
 					<div className="wideDiv">
-						<a href="/main.js">Home</a>
-						<a href="/introduction.js">Professional Team</a>
-						<a href="/progress.js">Gallery</a>
-            <a href = "/testimonials.js">Testimonials</a>
-            <a href = "/tips.js">Creative Tips</a>
-            <a href = "/contact.js">Contact</a>
+						<Link to ="/main.js"><a href="/main.js">Home</a></Link></Link>
+						<Link to ="/introduction.js"><a href="/introduction.js">Professional Team</a></Link></Link>
+            <Link to ="/progress.js"><a href="/progress.js">Gallery</a></Link></Link>
+            <Link to ="/testimonials.js"><a href = "/testimonials.js">Testimonials</a></Link></Link>
+            <Link to ="/tips.js"><a href = "/tips.js">Creative Tips</a></Link></Link>
+            <Link to ="/contact.js"><a href = "/contact.js">Contact</a></Link></Link>
 					</div>
 				</div>
 				<div className="navNarrow">
 					<i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
 					<div className="narrowLinks">
-						<a href="/main.js" onClick={this.burgerToggle}>Home</a>
-						<a href="/introduction.js" onClick={this.burgerToggle}>Professional Team</a>
-						<a href="/progress.js" onClick={this.burgerToggle}>Gallery</a>
-            <a href ="/testimonials.js" onClick={this.burgerToggle}>Testimonials</a>
-            <a href = "/tips.js" onClick={this.burgerToggle}>Creative Tips</a>
-            <a href = "/contact.js" onClick={this.burgerToggle}>Contact</a>
+						<Link to ="/main.js"><a href="/main.js" onClick={this.burgerToggle}>Home</a></Link>
+						<Link to ="/introduction.js"><a href="/introduction.js" onClick={this.burgerToggle}>Professional Team</a></Link>
+						<Link to ="/progress.js"><a href="/progress.js" onClick={this.burgerToggle}>Gallery</a></Link>
+            <Link to ="/testimonials.js"><a href ="/testimonials.js" onClick={this.burgerToggle}>Testimonials</a></Link>
+            <Link to ="/tips.js"><a href = "/tips.js" onClick={this.burgerToggle}>Creative Tips</a></Link>
+            <Link to ="/contact.js"><a href = "/contact.js" onClick={this.burgerToggle}>Contact</a></Link>
 					</div>
 				</div>
 			</nav>
