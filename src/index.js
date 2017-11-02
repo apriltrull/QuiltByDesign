cc05b94cd6c84115b944495ec604b37c6a4e2a11
April Trull <mtwoolybunnies@gmail.com>
2017-11-02 11:50:00 -0600

    navbar in place, working on styling for navbar

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavComponent from './components/heading.js';
+
 import Main from './components/main.js';
-import Contact from './components/contact.js';
+
 import './assets/stylesheets/index.css';
-import Progress from './components/progress.js';
 import Introduction from './components/introduction.js';
-import Tips from './components/tips.js';
-import Testimonials from './components/testimonials.js';
+
 
 class Grit extends React.Component {
   render() {
     return (
       <div>
-        <NavComponent/>  
+
         <Main />
         <Introduction/>
 
 
-         <Progress/>
-         <Testimonials/>
-         <Tips/>
-        <Contact />
+
       </div>
     );
   }
