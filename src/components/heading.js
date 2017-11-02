fce1c6b9ef49fe5a632d2b7ec7186bf6e54d814f
April Trull <mtwoolybunnies@gmail.com>
2017-11-02 11:42:45 -0600

    navbar in place, working on styling for navbar

--- src/components/heading.js
+++ src/components/heading.js
@@ -8,23 +8,23 @@ export default class NavComponent extends React.Component {
 			<nav>
 				<div className="navWide">
 					<div className="wideDiv">
-						<a href="./main.js">Home</a>
-						<a href="./introduction.js">Professional Team</a>
-						<a href="./progress.js">Gallery</a>
-            <a href = "./testimonials.js">Testimonials</a>
-            <a href = "./tips.js">Creative Tips</a>
-            <a href = "./contact.js">Contact</a>
+						<a href="./components/main.js">Home</a>
+						<a href="./components/introduction.js">Professional Team</a>
+						<a href="./components/progress.js">Gallery</a>
+            <a href = "./components/testimonials.js">Testimonials</a>
+            <a href = "./components/tips.js">Creative Tips</a>
+            <a href = "./components/contact.js">Contact</a>
 					</div>
 				</div>
 				<div className="navNarrow">
 					<i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
 					<div className="narrowLinks">
-						<a href="./main.js" onClick={this.burgerToggle}>Home</a>
-						<a href="./introduction.js" onClick={this.burgerToggle}>Professional Team</a>
-						<a href="./progress.js" onClick={this.burgerToggle}>Gallery</a>
-            <a href ="./testimonials.js" onClick={this.burgerToggle}>Testimonials</a>
-            <a href = "./tips.js" onClick={this.burgerToggle}>Creative Tips</a>
-            <a href = "./contact.js" onClick={this.burgerToggle}>Contact</a>
+						<a href="./components/main.js" onClick={this.burgerToggle}>Home</a>
+						<a href="./components/introduction.js" onClick={this.burgerToggle}>Professional Team</a>
+						<a href="./components/progress.js" onClick={this.burgerToggle}>Gallery</a>
+            <a href ="./components/testimonials.js" onClick={this.burgerToggle}>Testimonials</a>
+            <a href = "./components/tips.js" onClick={this.burgerToggle}>Creative Tips</a>
+            <a href = "./components/contact.js" onClick={this.burgerToggle}>Contact</a>
 					</div>
 				</div>
 			</nav>
