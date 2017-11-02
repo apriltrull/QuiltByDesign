0aa5475ef956723716ac45c46d8cd6dffc34eca9
April Trull <mtwoolybunnies@gmail.com>
2017-11-02 13:05:42 -0600

    navbar in place, working on styling for navbar

---
+++ src/callback/callback.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Callback extends Component {
+  render() {
+
+    return (
+      <div>
+        <h1>loading</h1>
+      </div>
+    );
+  }
+}
+
+export default Callback;
