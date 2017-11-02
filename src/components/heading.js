8d58e4e8b8965d236486d2d2a73c36add2bbdcdb
April Trull <mtwoolybunnies@gmail.com>
2017-11-02 09:47:54 -0600

    getting navbar to work

--- src/components/heading.js
+++ src/components/heading.js
@@ -9,17 +9,23 @@ export default class NavComponent extends React.Component {
 			<nav>
 				<div className="navWide">
 					<div className="wideDiv">
-						<a href="/main.js">Home</a>
-						<a href="/introduction.js">Owners</a>
-						<a href="/progress.js">Gallery</a>
+						<a href="./main.js">Home</a>
+						<a href="./introduction.js">Professional Team</a>
+						<a href="./progress.js">Gallery</a>
+            <a href = "./testimonials.js">Testimonials</a>
+            <a href = "./tips.js">Creative Tips</a>
+            <a href = "./contact.js">Contact</a>
 					</div>
 				</div>
 				<div className="navNarrow">
 					<i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
 					<div className="narrowLinks">
-						<a href="/main.js" onClick={this.burgerToggle}>Home</a>
-						<a href="/introduction.js" onClick={this.burgerToggle}>Owners</a>
-						<a href="/progress.js" onClick={this.burgerToggle}>Gallery</a>
+						<a href="./main.js" onClick={this.burgerToggle}>Home</a>
+						<a href="./introduction.js" onClick={this.burgerToggle}>Professional Team</a>
+						<a href="./progress.js" onClick={this.burgerToggle}>Gallery</a>
+            <a href ="./testimonials.js" onClick={this.burgerToggle}>Testimonials</a>
+            <a href = "./tips.js" onClick={this.burgerToggle}>Creative Tips</a>
+            <a href = "./contact.js" onClick={this.burgerToggle}>Contact</a>
 					</div>
 				</div>
 			</nav>
