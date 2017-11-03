4b0eb1422bddd6e7fb41d74ba396473845f10b6f
April Trull <mtwoolybunnies@gmail.com>
2017-11-03 08:36:23 -0600

    navbar in place, working on styling for navbar

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Router, Redirect } from 'react-router-dom';
 import Heading from './components/heading'; //now top level component
+import Main from './components/main';
 import Tips from './components/tips';
 import Contact from './components/contact';
 import Progress from './components/progress';
@@ -16,6 +17,7 @@ export const makeMainRoutes = () => {
       <div className="App">
         <Redirect from="/" to="/main" />
         <Route path="/"             render={(props) => <Heading {...props} />} />
+        <Router path="main"    render = {(props)}  => <Main {...props} /> />
         <Route path="/progress"     render={(props) => <Progress {...props} />} />
         <Route path="/testimonials"      render={(props) => <Testimonials {...props} />} />
         <Route path="/tips"        render={(props) => <Tips {...props }/>} />
