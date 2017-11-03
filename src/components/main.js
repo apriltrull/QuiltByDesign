c9af33483ccb8ba79fc001604368644e6ba60ca5
April Trull <mtwoolybunnies@gmail.com>
2017-11-03 10:55:44 -0600

    navbar in place, working on styling for navbar

--- src/components/main.js
+++ src/components/main.js
@@ -22,7 +22,7 @@ export default class Main extends React.Component {
 
             <div className = "advertise">
 
-              <h1 className="quilt">Quilting With Class</h1>
+              <h1 className="orange">Quilting With Class</h1>
               <h2 className = "extend">.....Professional Longarm Quilting by Becky Wilcox</h2>
             </div>
 
