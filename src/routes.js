af58ba29cfdcf58acce18ad7607e38bd33679a8f
April Trull <mtwoolybunnies@gmail.com>
2017-11-03 08:52:10 -0600

    navbar in place, working on styling for navbar

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Router, Redirect } from 'react-router-dom';
-import Heading from './components/heading'; //now top level component
 import Main from './components/main';
+import Heading from './components/heading'; //now top level component
+import Testimonials from './components/testimonials';
 import Tips from './components/tips';
 import Contact from './components/contact';
 import Progress from './components/progress';
-import  Testimonials from './components/testimonials';
+import  Introduction from './components/introduction';
 import History from './components/history';
 import './App.css';
 
@@ -16,8 +17,9 @@ export const makeMainRoutes = () => {
     <Router path="/main" history={History} component={Heading}>
       <div className="App">
         <Redirect from="/" to="/main" />
-        <Route path="/"             render={(props) => <Heading {...props} />} />
-        <Router path="main"    render = {(props)  => <Main {...props} />} />
+        <Route path="/"             render={(props) => <Heading  {...props} />} />
+        <Route path="/main"         render={(props) => <Main {...props} />} />
+        <Route path="/introduction" render={(props) => <Introduction {...props} />} />
         <Route path="/progress"     render={(props) => <Progress {...props} />} />
         <Route path="/testimonials"      render={(props) => <Testimonials {...props} />} />
         <Route path="/tips"        render={(props) => <Tips {...props }/>} />
