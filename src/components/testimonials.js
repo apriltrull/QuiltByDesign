172d9eb86f6603b087cba743fc34775f0525cf72
April Trull <mtwoolybunnies@gmail.com>
2017-11-06 11:15:50 -0700

    navbar up and working.  separate pages working. Bio, tips and tesimonials added.  Working on adding pdf file to tips page

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -10,8 +10,10 @@ export default class Testimonials extends React.Component {
 
               <h1 className='tags'>Testimonials</h1>
             <div className = "talk">
+            <ul>
 
-              <h2 className = "talk-text">"I found Becky to be very friendly and professional.  My quilts got done in a timely manner and she and Bill did a great job.   Im so pleased with the results.  I will be sending her all of my quilts."</h2>
+              <li>"Becky has done several quilts for me and I'm always impressed by her quality of work.  You can always tell that she has put in lots of time and energy to make each quilt special and unique.  Pam - NC"</li>
+              <li>I have many quilts by Becky Wilcox! I am in California and so they(the quilts) take the trip and come back with wonderful results!   I trust Becky's judgement and I have never been disappointed!  We connected on Facebook and actually went to high school together way back when!  So it is fun to do business now and I recommend her highly!</li>
             </div>
 
         </div>
