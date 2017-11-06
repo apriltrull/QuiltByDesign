438c5ced7f115de0586384f32f13aecc40885651
April Trull <mtwoolybunnies@gmail.com>
2017-11-05 18:44:21 -0700

    navbar in place, working on styling for navbar

--- src/components/tips.js
+++ src/components/tips.js
@@ -8,11 +8,11 @@ export default class Tips extends React.Component {
 
         <div className="creativity">
 
-              <h1 className='tags'>Beckys Creative Tip Corner</h1>
+              <h1 className='tags'>Tips and Tricks</h1>
             <div className = "tips">
 
-              <h2 className = "tips-text">A great way to keep your fabrics from stritching while quilting is to prewash your fabrics before using them.</h2>
-              <h2 className = "tips-text">Always square your quilts before sending them to your longarm quilter</h2>
+              <h2 className = "tips-text"> One of the problems as a longarm quilter that I run into fairly often are borders that flare or have extra bfabric in them.  Do I make a tuck? So I cut into the border and remove some of the fabric?  How can I quilt that flat???  Proper measuring is the answer.  Each time you add any border to the cneter of your quilt, follow the quidlines in the example below.  You will have a uch happiier longarm quilter, no headaches for her and your quilt will lokk so much nicer.</h2>
+              <h2 className = "tips-text">To pre-wash or not to pre-wash?  That is the quiestion.   there are many opinions on theat subject.  Mine has changedsince I began quilting.  When I bagan quilting, I never pre-washed.  It simplified things and I think I was a bit lazy.  I also loved that crisp feel of the fabric.  But since I have been pre-washing the fabric, my quilts have been so much more accureate.  I saw this proven in a class I went to.  The teacher showed samples of a simple nine patch block on a measuring grid.  The pre-washed fabric was right on the lines, nearly perfect.  The non-pre washed fabric wobbled and waved a bit.  And we all know that if the inaccuracies continue, they get bigger as the quilt gets bigger.  The end product is a less than square quilt.  To get the crispness back into your fabric, use a fabric spray such as May Wllen's Bes Press.  Better yet, make your own.  Mix 1 ounce of vodka, made from potao startch, with 8 ounces of distilled water.  Voila! Your own pressing agent and dfor pennies too!</h2>
               <h2 className = "tips-text">Iron your seams in the same direction, remembering to press, not pull on you fabrics</h2>
             </div>
 
