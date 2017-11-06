4947edeaba1862fe85a6a27b2c9ed28f4b2f83da
April Trull <mtwoolybunnies@gmail.com>
2017-11-06 11:19:48 -0700

    working on pdf file

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -14,7 +14,9 @@ export default class Testimonials extends React.Component {
 
               <li>"Becky has done several quilts for me and I'm always impressed by her quality of work.  You can always tell that she has put in lots of time and energy to make each quilt special and unique.  Pam - NC"</li>
               <li>I have many quilts by Becky Wilcox! I am in California and so they(the quilts) take the trip and come back with wonderful results!   I trust Becky's judgement and I have never been disappointed!  We connected on Facebook and actually went to high school together way back when!  So it is fun to do business now and I recommend her highly!</li>
-            </div>
+
+            </ul>
+           </div>
 
         </div>
 
