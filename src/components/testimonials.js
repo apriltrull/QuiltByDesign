b5a162257a08daab1f70c6ebdbd9c397cc6c21aa
April Trull <mtwoolybunnies@gmail.com>
2017-11-08 10:46:17 -0700

    styling testimonial and tips pages

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/stylesheets/testimonials.css';
 import Demo from '../assets/img/client-quilt3.JPG';
-// import Demo2 from '../assets/img/client-quilt2.PNG';
+
 
 export default class Testimonials extends React.Component {
 
@@ -25,8 +25,8 @@ export default class Testimonials extends React.Component {
 
                <div className="img">
                <img scr={Demo} alt="client quilt"/>
-              //  <img src={Demo2} alt="clients third quilt"/>
-               </div>
+
+             </div>
             </div>
 
            </div>
